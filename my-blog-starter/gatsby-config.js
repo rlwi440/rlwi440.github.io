2dba974d2f2b5586c7e6d1ebda021c0bbc5b7cd5
rlwi440 <rlwi440@naver.com>
2021-10-01 10:30:58 +0900

    robot.txt링크 추가연결

--- my-blog-starter/gatsby-config.js
+++ my-blog-starter/gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://rlwi440.github.io/',
+        sitemap: 'https://rlwi440.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',
